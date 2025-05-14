f06f288d3a6d2a37d6bb534b4ce8d1d424618e13
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-05-14 15:44:33 +0300

    Licensed.

--- lua/Flominal/Flominal.lua
+++ lua/Flominal/Flominal.lua
@@ -1,6 +1,4 @@
--- Flominal.lua
--- Yanked from TJ DeVries, thanks.
-
+-- Flominal  © 2025 by Emir Baha Yıldırım is licensed under CC BY-NC-SA 4.0. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/
 
 local M = {}
 
--- lua/Flominal/init.lua
+++ lua/Flominal/init.lua
@@ -1,3 +1,5 @@
+-- Flominal  © 2025 by Emir Baha Yıldırım is licensed under CC BY-NC-SA 4.0. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/
+
 local M = {}
 
 function M.setup(opts)
